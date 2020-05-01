62019dc0adf75244460c20ec17f8bb606f6e6dac
Joao Paulo Penalber <joao.p.penalber@gmail.com>
2020-05-01 15:13:39 -0300

    add highlights to cube

--- cube.cpp
+++ cube.cpp
@@ -3,8 +3,19 @@
 Cube::Cube() {
 }
 
+void Cube::select() {
+    selected = true;
+}
+
+void Cube::diselect() {
+    selected = false;
+}
+
 void Cube::setVertices(GLfloat centerPosX, GLfloat centerPosY, GLfloat centerPosZ, GLfloat edgeLength) {
     GLfloat halfSideLength = edgeLength * 0.5f;
+    this->edgeLength = edgeLength;
+
+    origin = {centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength};
 
     vertices =
     {
@@ -51,12 +62,35 @@ void Cube::Draw() {
     glEnableClientState( GL_VERTEX_ARRAY );
     glEnableClientState(GL_COLOR_ARRAY);
 
-    glVertexPointer( 3, GL_FLOAT, 0, (void*)&vertices[0] );
+    glVertexPointer(3, GL_FLOAT, 0, (void*)&vertices[0]);
     glColorPointer(3, GL_FLOAT, 0, (void*)&colour[0]);
 
     glDrawArrays( GL_QUADS, 0, 24 );
 
     glDisableClientState(GL_COLOR_ARRAY);
     glDisableClientState( GL_VERTEX_ARRAY );
+
+    if (selected) {
+        GLfloat cordV[] = {
+            origin[0], origin[1], origin[2],
+            origin[0]+2*edgeLength, origin[1], origin[2],
+
+            origin[0], origin[1], origin[2],
+            origin[0], origin[1]+2*edgeLength, origin[2],
+
+            origin[0], origin[1], origin[2],
+            origin[0], origin[1], origin[2]+2*edgeLength
+        };
+
+        glEnableClientState( GL_VERTEX_ARRAY );
+        glEnableClientState(GL_COLOR_ARRAY);
+
+        glVertexPointer( 3, GL_FLOAT, 0, cordV );
+
+        glDrawArrays( GL_LINES, 0, 6 );
+
+        glDisableClientState(GL_COLOR_ARRAY);
+        glDisableClientState( GL_VERTEX_ARRAY );
+    }
 
 }
--- cube.h
+++ cube.h
@@ -10,9 +10,17 @@ public:
     Cube();
     void setVertices(GLfloat centerPosX, GLfloat centerPosY, GLfloat centerPosZ, GLfloat edgeLength);
     void Draw();
+    void select();
+    void diselect();
 
 private:
     std::vector<GLfloat> vertices;
+
+    std::vector<GLfloat> origin;
+    GLfloat edgeLength;
+
+    bool selected = false;
+
     std::vector<GLfloat> colour =
     {
       255, 0, 0,
